a4c424253d920e7ed5aacb620468dfa966b6d54c
Weltgeist <narcisse118@gmail.com>
2020-07-16 13:07:47 -0400

    added field constructor and prompt-sync dependencies

--- main.js
+++ main.js
@@ -6,5 +6,16 @@ const fieldCharacter = '░';
 const pathCharacter = '*';
 
 class Field {
+  constructor(field) {
+    this._field = field;
+  }
   
 }
+
+const myField = new Field([
+  ['*', '░', 'O'],
+  ['░', 'O', '░'],
+  ['░', '^', '░'],
+]);
+
+console.log(myField._field);
