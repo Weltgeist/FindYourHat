74baa74b7d0ebecb44f41aba324ce6f784a2d3f9
Weltgeist <narcisse118@gmail.com>
2020-07-16 13:54:26 -0400

    added direction elem

--- main.js
+++ main.js
@@ -8,14 +8,30 @@ const pathCharacter = '*';
 class Field {
   constructor(field) {
     this._field = field;
+    this._direction = "";
   }
   get field(){
     return this._field;
   }
+  
+  get direction(){
+    return this._direction;
+  }
+  
+  set direction(direction){
+     this._direction = direction;
+  }
   print(){
     this.field.forEach(elem => console.log(elem.reduce((acc,curr) => acc += curr )));
   }
+  getWay(){
+    const way = prompt('Which way?');
+    this.direction = way;
+  }
 }
+
+
+
 
 const myField = new Field([
   ['*', '░', 'O'],
@@ -24,5 +40,5 @@ const myField = new Field([
 ]);
 
 console.log(myField._field);
-
+console.log(myField._curPos);
 myField.print()
